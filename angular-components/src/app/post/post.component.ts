a7deaae4ca9e29e5900b6bf00ffbf6fff652e7be
Arkadiy Kashin <akashin.nf@gmail.com>
2019-12-02 00:16:26 +0300

    angular components project

---
+++ angular-components/src/app/post/post.component.ts
@@ -0,0 +1,26 @@
+import {Component, ContentChild, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Post} from '../app.component';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  @Input() post: Post;
+  @Output() onRemove = new EventEmitter<number>();
+  @ContentChild('info', {static : true}) infoRef: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.infoRef.nativeElement);
+  }
+
+  removePost() {
+    this.onRemove.emit(this.post.id);
+  }
+
+
+}
